fb099587c3c0dc34dfeb8adcf71f815fec597f42
Vsevolod Livinskii <vsevolod.livinskij@frtk.ru>
2020-06-02 12:10:17 -0600

    Update copyright

--- src/expr.cpp
+++ src/expr.cpp
@@ -1,5 +1,6 @@
 /*
 Copyright (c) 2015-2020, Intel Corporation
+Copyright (c) 2019-2020, University of Utah
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
