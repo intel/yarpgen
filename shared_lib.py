baa34c36c6ff51ee7537c03c87fd161b8e694a2e
gbdngb12 <gbdngb12@naver.com>
2023-10-01 16:55:28 +0900

    update shared_library example

--- shared_lib.py
+++ shared_lib.py
@@ -1,19 +1,20 @@
 import ctypes
 
 # 라이브러리 로드
-example_strings = ctypes.CDLL('./libyarpgen.so')
+yarpgen_function = ctypes.CDLL('./libyarpgen.so')
 
 # 함수 프로토타입 정의
-example_strings.gen_yargen.argtypes = (ctypes.c_int, ctypes.POINTER(ctypes.c_char_p))
+yarpgen_function.gen_yarpgen.argtypes = (ctypes.c_int, ctypes.POINTER(ctypes.c_char_p))
+yarpgen_function = yarpgen_function.gen_yarpgen
 
-def call_print_string_array(str_list):
+def make_param(str_list):
     c_string_array = (ctypes.c_char_p * len(str_list))()
     for i, string in enumerate(str_list):
         c_string_array[i] = bytes(string, 'utf-8')
-    return example_strings.gen_yargen(len(str_list), c_string_array)
+    return len(str_list), c_string_array
 
 # 테스트
 if __name__ == "__main__":
-    test_strings = ["yarpgen", "--std=c","-o", "out"]
-    ret = call_print_string_array(test_strings)
+    test_strings = ["yarpgen", "--std=c", "-o", "out"]
+    ret = yarpgen_function(*make_param(test_strings))
     print(ret)
